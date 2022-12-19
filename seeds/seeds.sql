922774094d38caf886aa160024a40f3aa17f688c
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2022-12-19 00:43:51 -0600

    layout seeds

---
+++ seeds/seeds.sql
@@ -0,0 +1,43 @@
+
+-- Departments
+INSERT INTO department (name)
+VALUE ("Operations");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Nerds");
+INSERT INTO department (name)
+VALUE ("Human Capital");
+-- Add ids?
+
+
+-- Employees
+
+-- Roles
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Support Associate", 50000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Health Officer", 90000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Junior Dev", 65000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Less Junior Dev", 85000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "HR Associate", 55000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "HR Manager", 75000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Vice President", 125000, 5);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "Senior Vice President", 170000, 5);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "IT Guy", 80000, 6);
+
